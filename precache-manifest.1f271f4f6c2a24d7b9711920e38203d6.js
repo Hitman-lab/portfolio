22638fcd867dcea741c5324400852c83f5b83584
Hitman <hackzs@outlook.com>
2020-07-12 23:24:07 +0530

    Updates

---
+++ precache-manifest.1f271f4f6c2a24d7b9711920e38203d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be0a0fd40c68605981d5173afa5e2d1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bb31f88e2af0765b9612",
+    "url": "/portfolio/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "6a4644923c66e30d2dc0",
+    "url": "/portfolio/static/js/2.1a20d560.chunk.js"
+  },
+  {
+    "revision": "0261e2dee4a7e8969069abb914658242",
+    "url": "/portfolio/static/js/2.1a20d560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb31f88e2af0765b9612",
+    "url": "/portfolio/static/js/main.6a559319.chunk.js"
+  },
+  {
+    "revision": "8c78779c98e297eec2a1",
+    "url": "/portfolio/static/js/runtime-main.23e48518.js"
+  }
+]);
